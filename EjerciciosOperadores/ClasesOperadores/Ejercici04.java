d1a06a2320115c41e8ca0ab1efa10afa3fdd466d
IngAlim2023 <enrique.porras@udea.edu.co>
2024-07-22 17:38:46 -0500

    Modulo 1

--- EjerciciosOperadores/ClasesOperadores/Ejercici04.java
+++ EjerciciosOperadores/ClasesOperadores/Ejercici04.java
@@ -1,5 +1,12 @@
 package EjerciciosOperadores.ClasesOperadores;
 
 public class Ejercici04 {
-    
+    public static void main(String[] args) {
+        boolean estado = true;
+
+        // estado es un boleano, no se puede comparar con un entero
+        if (estado == false|| 2 > 1){
+            System.out.println("1. el estado es: " + estado);
+        }
+    }
 }
---
+++ EjerciciosOperadores/ClasesOperadores/Ejercici06.java
@@ -0,0 +1,36 @@
+package EjerciciosOperadores.ClasesOperadores;
+
+public class Ejercici06 {
+    public static void main(String[] args) {
+        int ladoCuadrado = 8;
+
+        // Calculo Área y perimetro de un cuadrado
+        
+        int areaCuadrado = ladoCuadrado * ladoCuadrado;
+        int perimetroCuadrado = ladoCuadrado * 4;
+
+        //Triangulo escaleno
+        double baseTriangulo = 9;
+        double alturaTriangulo = 8;
+        double areaTriangulo = baseTriangulo * alturaTriangulo *0.5;
+
+        //Asumo Triangulo Equilatero
+        double ladoUnoTriangulo = 8;
+        double ladoDosTriangulo = 8;
+        double perimetroTriangulo = ladoUnoTriangulo + ladoDosTriangulo*2;
+
+        // Calculos Rectangulo
+
+        double baseRectangulo = 8;
+        double alturaRectangulo = 6;
+        double areaRectangulo = baseRectangulo * alturaRectangulo;
+        double perimetroRectangulo = 2 * ( baseRectangulo + alturaRectangulo);
+
+        // Impresión de resultados
+        System.out.println("El área del cuadrado es: " + areaCuadrado + ", el perimetro es: " + perimetroCuadrado);
+        System.out.println("El área del triangulo es: " + areaTriangulo + ", el perimetro es: " + perimetroTriangulo);
+        System.out.println("El área del triangulo es: " + areaRectangulo + ", el perimetro es: " + perimetroRectangulo);
+
+
+    }
+}
